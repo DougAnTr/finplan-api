827f073ec5f44d774b2afc6e122a009a73dc848e
Douglas Trofino <douglast@outboxup.com>
2022-03-31 08:25:00 -0300

    chore: create disconnect function to mongodb

--- src/config/mongodbConnection.ts
+++ src/config/mongodbConnection.ts
@@ -4,3 +4,7 @@ import { constants } from './constants'
 export async function connect() {
   return await mongoose.connect(constants.database.mongo_url)
 }
+
+export async function disconnect() {
+  return await mongoose.disconnect()
+}
