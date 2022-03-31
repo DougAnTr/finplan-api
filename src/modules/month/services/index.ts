ca8c60e2e7d477b6d2b376f8502b5c98d7711879
Douglas Trofino <douglast@outboxup.com>
2022-03-31 10:47:25 -0300

    chore: create general import for month services

---
+++ src/modules/month/services/index.ts
@@ -0,0 +1,5 @@
+import {CreateMonthService} from './createMonth/createMonth.service'
+import {FindMonthService} from './findMonth/findMonth.service'
+import {ListMonthService} from './listMonth/listMonth.service'
+
+export {CreateMonthService, FindMonthService, ListMonthService}
