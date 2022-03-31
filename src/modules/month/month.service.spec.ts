0671a04a4048dd69d68a027b4e55082028ba0ad7
Douglas Trofino <douglast@outboxup.com>
2022-03-31 06:45:22 -0300

    Revert "chore: changing jest for mocha"
    This reverts commit 3306787344fb76207c6bfbfde6758b04b332adc9.

---
+++ src/modules/month/month.service.spec.ts
@@ -0,0 +1,39 @@
+import { mongoose } from '@typegoose/typegoose'
+
+import { constants } from '../../config/constants'
+
+import { MonthService } from './month.service'
+
+const makeSut = () => {
+  const sut = new MonthService()
+
+  return {sut}
+}
+
+describe('Month Service', () => {
+
+  beforeAll(async() => {
+    await mongoose.connect(`mongodb://localhost:${constants.database.port}/${constants.database.name}`)
+  })
+
+  afterAll(async() => {
+    await mongoose.disconnect()
+  })
+
+
+  describe('create', () => {
+    it('should return the created month', async () => {
+      const {sut} = makeSut()
+  
+      const testMonth = {
+        number: 0,
+        year: 2023
+      }
+  
+      const month = await sut.create(testMonth)
+  
+      expect(month.number).toBe(testMonth.number)
+      expect(month.year).toBe(testMonth.year)
+    })
+  })
+})
